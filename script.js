0434ed66533d83d71ddde63de8e572904c226257
dune123 <96974209+dune123@users.noreply.github.com>
2024-11-01 13:14:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const valueP=document.getElementById('countContainer')
+
+const increment=document.getElementById('increment')
+
+increment.addEventListener('click',()=>{
+	let x=parseInt(valueP.innerText);
+    x=x+1;
+    valueP.innerText=x;
+})
