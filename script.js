918a4d9aa6f13d04b9d889e980a9f9f32bc0f87b
dune123 <96974209+dune123@users.noreply.github.com>
2024-11-01 16:14:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
 const valueP=document.getElementById('countContainer')
 
-const increment=document.getElementById('increment')
+const increment=document.getElementById('incrementBtn')
 
 increment.addEventListener('click',()=>{
-	let x=parseInt(valueP.innerText);
-    x=x+1;
-    valueP.innerText=x;
+	let x = parseInt(valueP.innerText);
+    x = x + 1;
+    valueP.innerText = x;
 })
